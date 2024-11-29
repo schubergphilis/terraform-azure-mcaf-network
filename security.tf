71794e08e108614f65c2069fc1fc27ae4d679855
fblankendaal <fblankendaal@schubergphilis.com>
2024-11-29 12:22:55 +0100

    fix for simple fw rules

--- security.tf
+++ security.tf
@@ -53,19 +53,41 @@ resource "azurerm_network_security_group" "simple" {
 }
 
 resource "azurerm_network_security_rule" "simple" {
-  for_each = local.preprocessed_security_rules
+  for_each = {
+    for item, rule in local.nsg_with_default_security_rules : lower("${rule.subnet_key}_${rule.priority}_${rule.access}_${rule.direction}") => rule
+  }
 
-  name                        = each.value.name
-  priority                    = each.value.priority
-  direction                   = each.value.direction
-  access                      = each.value.access
-  protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = azurerm_network_security_group.this.resource_group_name
-  network_security_group_name = azurerm_network_security_group.simple[each.value.subnet_key].name
+  access                                     = each.value.access
+  direction                                  = each.value.direction
+  name                                       = each.value.name
+  network_security_group_name                = azurerm_network_security_group.additional[each.value.subnet_key].name
+  priority                                   = each.value.priority
+  protocol                                   = each.value.protocol
+  resource_group_name                        = azurerm_network_security_group.this.resource_group_name
+  description                                = each.value.description
+  destination_address_prefix                 = each.value.destination_address_prefix
+  destination_address_prefixes               = each.value.destination_address_prefixes
+  destination_application_security_group_ids = each.value.destination_application_security_group_ids
+  destination_port_range                     = each.value.destination_port_range
+  destination_port_ranges                    = each.value.destination_port_ranges
+  source_address_prefix                      = each.value.source_address_prefix
+  source_address_prefixes                    = each.value.source_address_prefixes
+  source_application_security_group_ids      = each.value.source_application_security_group_ids
+  source_port_range                          = each.value.source_port_range
+  source_port_ranges                         = each.value.source_port_ranges
+
+  dynamic "timeouts" {
+    for_each = each.value.timeouts == null ? [] : [each.value.timeouts]
+    content {
+      create = timeouts.value.create
+      delete = timeouts.value.delete
+      read   = timeouts.value.read
+      update = timeouts.value.update
+    }
+  }
+
+  # Do not remove this `depends_on` block. It is required to ensure the NSG is created before the rule.
+  depends_on = [azurerm_network_security_group.simple]
 }
 
 resource "azurerm_subnet_network_security_group_association" "simple" {
