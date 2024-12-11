fa9860d674deb53e24d50b4d3920524224caf144
fblankendaal <fblankendaal@schubergphilis.com>
2024-12-11 11:01:07 +0100

    add missing fields for default rules

--- security.tf
+++ security.tf
@@ -14,17 +14,23 @@ resource "azurerm_network_security_group" "this" {
 resource "azurerm_network_security_rule" "default" {
   for_each = local.security_rules
 
-  name                        = each.value.name
-  priority                    = each.value.priority
-  direction                   = each.value.direction
-  access                      = each.value.access
-  protocol                    = each.value.protocol
-  source_port_range           = each.value.source_port_range
-  destination_port_range      = each.value.destination_port_range
-  source_address_prefix       = each.value.source_address_prefix
-  destination_address_prefix  = each.value.destination_address_prefix
-  resource_group_name         = azurerm_network_security_group.this.resource_group_name
-  network_security_group_name = azurerm_network_security_group.this.name
+  name                                       = each.value.name
+  priority                                   = each.value.priority
+  direction                                  = each.value.direction
+  access                                     = each.value.access
+  protocol                                   = each.value.protocol
+  source_port_range                          = each.value.source_port_range
+  source_port_ranges                         = each.value.source_port_ranges
+  destination_port_range                     = each.value.destination_port_range
+  destination_port_ranges                    = each.value.destination_port_ranges
+  destination_application_security_group_ids = each.value.destination_application_security_group_ids
+  source_address_prefix                      = each.value.source_address_prefix
+  source_address_prefixes                    = each.value.source_address_prefixes
+  source_application_security_group_ids      = each.value.source_application_security_group_ids
+  destination_address_prefix                 = each.value.destination_address_prefix
+  destination_address_prefixes               = each.value.destination_address_prefixes
+  resource_group_name                        = azurerm_network_security_group.this.resource_group_name
+  network_security_group_name                = azurerm_network_security_group.this.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
